bd92fb30d6ab03d4d5cc679425458abbd3c788d0
Yuri Nogueira <personal@yurinogueira.dev.br>
2023-07-09 17:31:45 -0300

    chore: adjust trabalho 06

--- trabalho06-api/src/main/java/br/dev/yurinogueira/repository/FuncaoRepository.java
+++ trabalho06-api/src/main/java/br/dev/yurinogueira/repository/FuncaoRepository.java
@@ -2,7 +2,9 @@ package br.dev.yurinogueira.repository;
 
 import br.dev.yurinogueira.model.Funcao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FuncaoRepository extends JpaRepository<Funcao, Long> {
 
 }
--- trabalho06-api/src/main/java/br/dev/yurinogueira/repository/PessoaRepository.java
+++ trabalho06-api/src/main/java/br/dev/yurinogueira/repository/PessoaRepository.java
@@ -1,15 +1,13 @@
 package br.dev.yurinogueira.repository;
 
 import br.dev.yurinogueira.model.Pessoa;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
     @Query("SELECT p FROM Pessoa p WHERE p.id IN (SELECT f.pessoa.id FROM Funcao f WHERE f.id = :id)")
